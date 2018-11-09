6357f25d83ed87d6b980ebe4a62ae0057c18e7c8
Mat V <mvincent.it@gmail.com>
2018-11-09 23:32:18 +0100

    Add custom logo

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -141,7 +141,7 @@ module.exports = (options) => webpackMerge(commonConfig({ env: ENV }), {
         ]),
         new WebpackNotifierPlugin({
             title: 'JHipster',
-            contentImage: path.join(__dirname, 'logo-jhipster.png')
+            contentImage: path.join(__dirname, 'LOGO.png')
         })
     ].filter(Boolean),
     mode: 'development'

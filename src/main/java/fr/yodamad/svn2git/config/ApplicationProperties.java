c69992efc918350ffd8a5658e893c7d91668e4b4
Mat V <mvincent.it@gmail.com>
2019-07-15 22:22:11 +0200

    :wrench: Add configuration keys

--- src/main/java/fr/yodamad/svn2git/config/ApplicationProperties.java
+++ src/main/java/fr/yodamad/svn2git/config/ApplicationProperties.java
@@ -2,6 +2,8 @@ package fr.yodamad.svn2git.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+
 /**
  * Properties specific to Svn 2 Git.
  * <p>
@@ -14,11 +16,78 @@ public class ApplicationProperties {
     public Work work = new Work();
     public Svn svn = new Svn();
     public Gitlab gitlab = new Gitlab();
+    public Git git = new Git();
+    public Artifactory artifactory = new Artifactory();
     public Password password = new Password();
     public Override override = new Override();
     public Flags flags = new Flags();
 
+    public Work getWork() {
+        return work;
+    }
+
+    public void setWork(Work work) {
+        this.work = work;
+    }
+
+    public Svn getSvn() {
+        return svn;
+    }
+
+    public void setSvn(Svn svn) {
+        this.svn = svn;
+    }
+
+    public Artifactory getArtifactory() {
+        return artifactory;
+    }
+
+    public void setArtifactory(Artifactory artifactory) {
+        this.artifactory = artifactory;
+    }
+
+    public Git getGit() {
+        return git;
+    }
+
+    public void setGit(Git git) {
+        this.git = git;
+    }
+
+    public Gitlab getGitlab() {
+        return gitlab;
+    }
+
+    public void setGitlab(Gitlab gitlab) {
+        this.gitlab = gitlab;
+    }
+
+    public Password getPassword() {
+        return password;
+    }
+
+    public void setPassword(Password password) {
+        this.password = password;
+    }
+
+    public Override getOverride() {
+        return override;
+    }
+
+    public void setOverride(Override override) {
+        this.override = override;
+    }
+
+    public Flags getFlags() {
+        return flags;
+    }
+
+    public void setFlags(Flags flags) {
+        this.flags = flags;
+    }
+
     public static class Work {
+
         public String directory = System.getenv("java.io.tmpdir");
 
         public Integer maxSvnLevel = 3;
@@ -31,19 +100,31 @@ public class ApplicationProperties {
             this.directory = directory;
         }
 
-        public Integer getMaxSvnLevel() { return maxSvnLevel; }
+        public Integer getMaxSvnLevel() {
+            return maxSvnLevel;
+        }
 
-        public void setMaxSvnLevel(Integer maxSvnLevel) { this.maxSvnLevel = maxSvnLevel; }
+        public void setMaxSvnLevel(Integer maxSvnLevel) {
+            this.maxSvnLevel = maxSvnLevel;
+        }
     }
 
     public static class Svn {
-        /** SVN default user. */
+        /**
+         * SVN default user.
+         */
         public String user;
-        /** SVN default password. */
+        /**
+         * SVN default password.
+         */
         public String password;
-        /** SVN Url. */
+        /**
+         * SVN Url.
+         */
         public String url;
-        /** Flag for credentials. */
+        /**
+         * Flag for credentials.
+         */
         public String credentials;
 
         public String getUser() {
@@ -80,16 +161,30 @@ public class ApplicationProperties {
     }
 
     public static class Gitlab {
-        /** Gitlab Url. */
+        /**
+         * Gitlab Url.
+         */
         public String url;
-        /** Gitlab service account. */
+        /**
+         * Gitlab service account.
+         */
         public String account;
-        /** Gitlab access token. */
+        /**
+         * Gitlab access token.
+         */
         public String token;
-        /** Flag for credentials. */
+        /**
+         * Flag for credentials.
+         */
         public String credentials;
-        /** Max waiting time. */
+        /**
+         * Max waiting time.
+         */
         public Integer wait;
+        /**
+         * Dynamic Local Config
+         */
+        public List<String> dynamicLocalConfig;
 
         public String getUrl() {
             return url;
@@ -130,6 +225,112 @@ public class ApplicationProperties {
         public void setWait(Integer wait) {
             this.wait = wait;
         }
+
+        public List<String> getDynamicLocalConfig() {
+            return dynamicLocalConfig;
+        }
+
+        public void setDynamicLocalConfig(List<String> dynamicLocalConfig) {
+            this.dynamicLocalConfig = dynamicLocalConfig;
+        }
+    }
+
+    public static class Artifactory {
+        /**
+         * Artifactory support enablement flag.
+         */
+        public Boolean enabled;
+        /**
+         * Artifactory Url.
+         */
+        public String url;
+        /**
+         * Artifactory user.
+         */
+        public String user;
+        /**
+         * Artifactory password.
+         */
+        public String password;
+        /**
+         * Artifactory default repository.
+         */
+        public String repository;
+        /**
+         * Artifactory groupId prefix.
+         */
+        public String groupIdPrefix;
+
+        /**
+         * Used to delete contents of folder after pushing to artifactory
+         * However bfg does not permit us to specify folder path (just a folder name)
+         */
+        public String deleteFolderWithBFG;
+
+        /**
+         * Binaries directory.
+         */
+        public String binariesDirectory;
+
+        public Boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(Boolean enabled) {
+            this.enabled = enabled;
+        }
+
+        public String getUser() {
+            return user;
+        }
+
+        public void setUser(String user) {
+            this.user = user;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getRepository() {
+            return repository;
+        }
+
+        public void setRepository(String repository) {
+            this.repository = repository;
+        }
+
+        public String getGroupIdPrefix() {
+            return groupIdPrefix;
+        }
+
+        public void setGroupIdPrefix(String groupIdPrefix) {
+            this.groupIdPrefix = groupIdPrefix;
+        }
+
+        public String getBinariesDirectory() {
+            return binariesDirectory;
+        }
+
+        public void setBinariesDirectory(String binariesDirectory) {
+            this.binariesDirectory = binariesDirectory;
+        }
+
+        public String getDeleteFolderWithBFG() {return deleteFolderWithBFG;}
+
+        public void setDeleteFolderWithBFG(String deleteFolderWithBFG) {this.deleteFolderWithBFG = deleteFolderWithBFG;}
     }
 
     public static class Password {
@@ -165,8 +366,61 @@ public class ApplicationProperties {
         }
     }
 
+    public static class Git {
+        /**
+         * add -f (force) option when executing git mv
+         **/
+        public boolean forceMvOnError = false;
+
+        /**
+         * add -k (skip) option when executing git mv
+         **/
+        public boolean skipMvOnError = false;
+        public boolean preserveEmptyDirs = false;
+
+        public boolean isPreserveEmptyDirs() {
+            return preserveEmptyDirs;
+        }
+
+        public void setPreserveEmptyDirs(boolean preserveEmptyDirs) {
+            this.preserveEmptyDirs = preserveEmptyDirs;
+        }
+
+        public boolean isForceMvOnError() {
+            return forceMvOnError;
+        }
+
+        public void setForceMvOnError(boolean forceMvOnError) {
+            this.forceMvOnError = forceMvOnError;
+        }
+
+        public boolean isSkipMvOnError() {
+            return skipMvOnError;
+        }
+
+        public void setSkipMvOnError(boolean skipMvOnError) {
+            this.skipMvOnError = skipMvOnError;
+        }
+    }
+
     public static class Flags {
+
         public boolean projectCleaningOption = false;
+
+        public boolean gitlabGroupCreationOption = false;
+
+        /**
+         * Cleanup Working Diretory when finished
+         */
+        public Boolean cleanupWorkDirectory;
+
+        public Boolean getCleanupWorkDirectory() {
+            return cleanupWorkDirectory;
+        }
+
+        public void setCleanupWorkDirectory(Boolean cleanupWorkDirectory) {
+            this.cleanupWorkDirectory = cleanupWorkDirectory;
+        }
 
         public boolean isProjectCleaningOption() {
             return projectCleaningOption;
@@ -175,49 +429,13 @@ public class ApplicationProperties {
         public void setProjectCleaningOption(boolean projectCleaningOption) {
             this.projectCleaningOption = projectCleaningOption;
         }
+
+        public boolean isGitlabGroupCreationOption() {
+            return gitlabGroupCreationOption;
+        }
+
+        public void setGitlabGroupCreationOption(boolean gitlabGroupCreationOption) {
+            this.gitlabGroupCreationOption = gitlabGroupCreationOption;
+        }
     }
-
-    public Work getWork() {
-        return work;
-    }
-
-    public void setWork(Work work) {
-        this.work = work;
-    }
-
-    public Svn getSvn() {
-        return svn;
-    }
-
-    public void setSvn(Svn svn) {
-        this.svn = svn;
-    }
-
-    public Gitlab getGitlab() {
-        return gitlab;
-    }
-
-    public void setGitlab(Gitlab gitlab) {
-        this.gitlab = gitlab;
-    }
-
-    public Password getPassword() {
-        return password;
-    }
-
-    public void setPassword(Password password) {
-        this.password = password;
-    }
-
-    public Override getOverride() {
-        return override;
-    }
-
-    public void setOverride(Override override) {
-        this.override = override;
-    }
-
-    public Flags getFlags() { return flags; }
-
-    public void setFlags(Flags flags) { this.flags = flags; }
 }

31227bfb4f6c845439bfdfdb869702aa8677b9d4
Mat V <mvincent.it@gmail.com>
2020-11-12 22:38:17 +0100

    💃 Init migration to Kotlin

---
+++ src/main/kotlin/fr/yodamad/svn2git/web/rest/GitlabResource.kt
@@ -0,0 +1,205 @@
+package fr.yodamad.svn2git.web.rest
+
+import com.codahale.metrics.annotation.Timed
+import fr.yodamad.svn2git.config.ApplicationProperties
+import fr.yodamad.svn2git.domain.GitlabInfo
+import fr.yodamad.svn2git.domain.Migration
+import fr.yodamad.svn2git.service.util.GitlabAdmin
+import fr.yodamad.svn2git.service.util.MigrationConstants
+import org.apache.commons.lang3.StringUtils
+import org.gitlab4j.api.GitLabApi
+import org.gitlab4j.api.GitLabApiException
+import org.gitlab4j.api.UserApi
+import org.gitlab4j.api.models.Group
+import org.gitlab4j.api.models.Project
+import org.gitlab4j.api.models.User
+import org.gitlab4j.api.models.Visibility
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+import java.util.*
+import java.util.stream.IntStream
+
+/**
+ * Controller to use Gitlab API
+ */
+@RestController
+@RequestMapping("/api/gitlab/")
+open class GitlabResource(val gitlabAdmin: GitlabAdmin,
+                          val applicationProperties: ApplicationProperties) {
+
+    val logger: Logger = LoggerFactory.getLogger(GitlabResource::class.java)
+
+    /**
+     * Check if a user exists on Gitlab
+     *
+     * @param userName User ID search
+     * @return if user found
+     */
+    @Timed
+    @PostMapping("user/{username}")
+    open fun checkUser(@PathVariable("username") userName: String?, @RequestBody gitlabInfo: GitlabInfo): ResponseEntity<Boolean?>? {
+        val api: UserApi = overrideGitlab(gitlabInfo).userApi
+        val user = api.getOptionalUser(userName)
+        val exception = GitLabApi.getOptionalException(user)
+        if (exception != null) {
+            logger.error("Fail to access gitlab", exception)
+            return ResponseEntity.badRequest().build()
+        }
+        return if (user.isPresent) {
+            ResponseEntity.ok().body(user.isPresent)
+        } else {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    /**
+     * Check if a group exists on Gitlab, AND that the passed in userName is a member of that group
+     *
+     * @param groupName Group name search
+     * @param userName  Member of group we are checking for
+     * @return if group found
+     */
+    @Timed
+    @PostMapping("group/{groupName}/members/{userName}")
+    open fun checkGroup(@PathVariable("groupName") groupName: String?,
+                        @PathVariable("userName") userName: String?,
+                        @RequestBody gitlabInfo: GitlabInfo): ResponseEntity<Boolean>? {
+        val gitlab = overrideGitlab(gitlabInfo)
+        // Group where project will be created
+        val group = gitlab.groupApi.getOptionalGroup(groupName)
+
+        // User that will be used to create a project
+        var user: Optional<User>?
+        // if token is blank / emtpy, we use the default gitlab user
+        user = if (StringUtils.isBlank(gitlabInfo.token)) {
+            gitlab.userApi.getOptionalUser(applicationProperties.gitlab.account)
+        } else {
+            // Username is the username that is checked in first step of wizard
+            gitlab.userApi.getOptionalUser(userName)
+        }
+
+        // if the group is present and the user that will use it is present
+        return if (group.isPresent && user.isPresent) {
+            try {
+
+                // check if userName passed in is a member of the group (includingInherited)
+                // An exception is thrown if member is not found in group.
+                val member = gitlab.groupApi.getMember(group.get().id, user.get().id)
+                if (member != null) {
+                    logger.info(String.format("User:%s is a member of the target group:%s", user.get().username, group.get().name))
+
+                    // Group exists and the passed in username is a member of it
+                    ResponseEntity.ok().body(group.isPresent)
+                } else {
+                    // This should not be possible
+                    logger.error(String.format("Member:%s not found in group:%s",
+                        user.get().username, group.get().name))
+
+                    // Group exists but an unexpected error getting user
+                    ResponseEntity.notFound().build()
+                }
+            } catch (apiEx: GitLabApiException) {
+
+                // just in case token in message
+                val message = apiEx.message!!.replace(applicationProperties.gitlab.token, MigrationConstants.STARS)
+                if (apiEx.reason.equals("Not Found", ignoreCase = true)) {
+                    // UserName not found : is a possible case. i.e. we only raise a warning in logs.
+                    logger.warn(String.format("Member:%s not found in group:%s",
+                        user.get().username, group.get().name))
+                } else {
+                    logger.error("Error getting member of group", message)
+                }
+
+                // User didn't exist in group
+                ResponseEntity.notFound().build()
+            }
+        } else {
+            // Group didn't exist or user didn't exist
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    /**
+     * Creates a group on Gitlab
+     *
+     * @param groupName Group name to create
+     * @return
+     */
+    @Timed
+    @PutMapping("group/{groupName}")
+    open fun createGroup(@PathVariable("groupName") groupName: String?, @RequestBody gitlabInfo: GitlabInfo?): ResponseEntity<Boolean>? {
+        val group = Group()
+        group.name = groupName
+        group.path = groupName
+        group.visibility = Visibility.INTERNAL
+        return try {
+            overrideGitlab(gitlabInfo!!).groupApi.addGroup(group)
+            ResponseEntity.ok().body(true)
+        } catch (apiEx: GitLabApiException) {
+            ResponseEntity.badRequest().build()
+        }
+    }
+
+    /**
+     * Override Gitlab default configuration if necessary
+     * @param gitlabInfo Received gitlab info
+     * @return Initialized API for Gitlab
+     */
+    open fun overrideGitlab(gitlabInfo: GitlabInfo): GitLabApi {
+        val gitlab = gitlabAdmin
+
+        // If gitlabInfo.token is empty assure using values found in application.yml.
+        // i.e. those in default GitlabAdmin object
+        if (StringUtils.isEmpty(gitlabInfo.token)) {
+            logger.info("Already using default url and token")
+        } else {
+            // If gitlabInfo.token has a value we overide as appropriate
+            if (!gitlab.api().gitLabServerUrl.equals(gitlabInfo.url, ignoreCase = true) ||
+                !gitlab.api().authToken.equals(gitlabInfo.token, ignoreCase = true)) {
+                logger.info("Overiding gitlab url and token")
+                val api = GitLabApi(gitlabInfo.url, gitlabInfo.token)
+                api.ignoreCertificateErrors = true
+                return api
+            }
+        }
+        return gitlab.api()
+    }
+
+    /**
+     * Remove an existing group
+     *
+     * @param migration Migration containing group information
+     * @throws GitLabApiException Problem when removing
+     */
+    @Throws(GitLabApiException::class)
+    open fun removeGroup(migration: Migration) {
+
+        //Get default GitlabAdmin object
+        try {
+            val elements = migration.svnProject.split("/").toTypedArray()
+            val namespace = StringBuffer(migration.gitlabGroup)
+            IntStream.range(1, elements.size)
+                .forEach { i: Int -> namespace.append(String.format("/%s", elements[i])) }
+            val projects = gitlabAdmin.projectApi().getProjects(elements[elements.size - 1])
+            val project = projects.stream().filter { p: Project -> p.pathWithNamespace.equals(namespace.toString(), ignoreCase = true) }.findFirst().get()
+            gitlabAdmin.projectApi().deleteProject(project)
+            // Waiting for gitlab to delete it completely
+            val maxAge = LocalDateTime.now().plus(applicationProperties.gitlab.waitSeconds.toLong(), ChronoUnit.SECONDS)
+            while (gitlabAdmin.projectApi().getProject(project.id) != null || maxAge.isAfter(LocalDateTime.now())) {
+            }
+        } catch (apiEx: GitLabApiException) {
+            if (apiEx.reason.equals("Not Found", ignoreCase = true)) {
+                // Project already deleted
+                logger.info("Unknown project, cannot delete it (or maybe already deleted)")
+                return
+            }
+            val message = apiEx.message!!.replace(applicationProperties.gitlab.token, MigrationConstants.STARS)
+            logger.error("Impossible to remove group", message)
+            throw apiEx
+        }
+    }
+}
---
+++ src/main/kotlin/fr/yodamad/svn2git/web/rest/SvnResource.kt
@@ -0,0 +1,171 @@
+package fr.yodamad.svn2git.web.rest
+
+import com.codahale.metrics.annotation.Timed
+import fr.yodamad.svn2git.config.ApplicationProperties
+import fr.yodamad.svn2git.domain.SvnInfo
+import fr.yodamad.svn2git.domain.SvnStructure
+import fr.yodamad.svn2git.domain.SvnStructure.FakeModule
+import fr.yodamad.svn2git.domain.SvnStructure.SvnModule
+import org.apache.commons.lang3.StringUtils
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.tmatesoft.svn.core.*
+import org.tmatesoft.svn.core.auth.ISVNAuthenticationManager
+import org.tmatesoft.svn.core.internal.wc.DefaultSVNAuthenticationManager
+import org.tmatesoft.svn.core.wc.SVNRevision
+import org.tmatesoft.svn.core.wc2.ISvnObjectReceiver
+import org.tmatesoft.svn.core.wc2.SvnOperationFactory
+import org.tmatesoft.svn.core.wc2.SvnTarget
+import java.util.*
+import java.util.function.Consumer
+
+fun keywords() : MutableList<String> {
+    val mutableList: MutableList<String> = ArrayList(3)
+    mutableList.add("trunk")
+    mutableList.add("branches")
+    mutableList.add("tags")
+    return mutableList;
+}
+
+@RestController
+@RequestMapping("/api/svn/")
+open class SvnResource(val applicationProperties: ApplicationProperties) {
+
+    /** Logger.  */
+    private val log = LoggerFactory.getLogger(SvnResource::class.java)
+
+    /**
+     * Check if a SVN repository
+     * @param repositoryName SVN repository ID search
+     * @return if repository found
+     */
+    @Timed
+    @PostMapping("repository/{repositoryName}")
+    open fun checkSVN(
+        @PathVariable("repositoryName") repositoryName: String?,
+        @RequestParam("depth") depth: Int?,
+        @RequestBody svnInfo: SvnInfo): ResponseEntity<SvnStructure?>? {
+        val structure: SvnStructure = listSVN(svnInfo, repositoryName, depth)
+
+        // Repository not found case
+        return if (!structure.flat && structure.modules.isEmpty()) {
+            ResponseEntity.notFound().build()
+        } else ResponseEntity.ok().body(structure)
+    }
+
+    /**
+     * List directories in root folder of SVN repository
+     * @param svnInfo SVN info to use
+     * @param repo Repository to explore
+     * @return list of directories found
+     */
+    protected open fun listSVN(svnInfo: SvnInfo, repo: String?, depth: Int?): SvnStructure {
+        val depthOrDefault = depth ?: 1
+        val structure = SvnStructure(repo)
+        structure.modules = listModulesSVN(svnInfo, repo, null, 0, depthOrDefault)
+        if (structure.modules.stream().anyMatch { m: SvnModule? -> m is FakeModule }) {
+            structure.root = true
+            structure.modules.clear()
+        }
+        structure.flat = structure.modules.isEmpty()
+        log.info("SVN structure found : {}", structure)
+        return structure
+    }
+
+    /**
+     * List modules into SVN structure
+     * @param svnInfo Current svn info
+     * @param repo Current svn repository
+     * @param module Current module inspected
+     * @return Complete module structure
+     */
+    protected open fun listModulesSVN(
+        svnInfo: SvnInfo, repo: String?, module: SvnModule?, level: Int, maxDepth: Int): List<SvnModule>? {
+        if (level == maxDepth) {
+            log.info("Reaching max levels authorized for discovery, stop here")
+            return emptyList()
+        }
+        val modules: MutableList<SvnModule> = ArrayList()
+        if (module == null) {
+            log.info("Check for modules in {}", String.format("%s/%s", svnInfo.url, repo))
+        } else {
+            log.info("Check for modules in {}", module)
+        }
+        val revision = SVNRevision.HEAD
+        val operationFactory = SvnOperationFactory()
+
+        // Set authentication if needed
+        var authManager: ISVNAuthenticationManager? = DefaultSVNAuthenticationManager(null, true, svnInfo.user, svnInfo.password, null, null)
+        if (!StringUtils.isEmpty(applicationProperties.svn.user)) {
+            authManager = DefaultSVNAuthenticationManager(null, true,
+                applicationProperties.svn.user, applicationProperties.svn.password, null, null)
+            operationFactory.authenticationManager = authManager
+        }
+        operationFactory.authenticationManager = authManager
+        val list = operationFactory.createList()
+        list.depth = SVNDepth.IMMEDIATES
+        list.revision = revision
+        try {
+            val svnUrl = if (svnInfo.url.endsWith("/")) svnInfo.url else String.format("%s/", svnInfo.url)
+            if (module == null) {
+                list.addTarget(SvnTarget.fromURL(SVNURL.parseURIEncoded(String.format("%s%s", svnUrl, repo)), revision))
+            } else {
+                list.addTarget(SvnTarget.fromURL(SVNURL.parseURIEncoded(String.format("%s%s%s", svnUrl, repo, module.path)), revision))
+            }
+        } catch (e: SVNException) {
+            log.error("Cannot list SVN", e)
+        }
+        val modulesFounds: MutableList<SvnModule> = ArrayList()
+        list.receiver = ISvnObjectReceiver { target: SvnTarget?, `object`: SVNDirEntry ->
+            val name = `object`.relativePath
+            if (name != null && !name.isEmpty() && !keywords().contains(name)) {
+
+                // found a directory
+                if (`object`.kind == SVNNodeKind.DIR) {
+                    if (module == null) {
+                        log.debug("Adding SVN module {}", name)
+                        modulesFounds.add(SvnModule(name, ""))
+                    } else if (!module.flat) {
+                        log.debug("Adding SVN submodule {} in {}", name, module)
+                        modulesFounds.add(SvnModule(name, module.path))
+                    }
+                } else if (`object`.kind == SVNNodeKind.FILE && module != null) {
+                    // file case : module may be flat, stop searching
+                    module.flat = true
+                    // remove potential folders previously found for this module
+                    modulesFounds.clear()
+                }
+            }
+            if (name != null && !name.isEmpty() && keywords().contains(name)) {
+                if (module != null) {
+                    log.info(String.format("Module %s with layout %s", module.name, name))
+                    module.layoutElements.add(name)
+                } else {
+                    // Root level case
+                    modulesFounds.add(FakeModule())
+                }
+            }
+        }
+        try {
+            list.run()
+        } catch (ex: SVNException) {
+            log.error("Cannot list SVN", ex)
+        }
+        modulesFounds.stream()
+            .filter { m: SvnModule? -> m !is FakeModule }
+            .map { e: SvnModule -> modules.add(e) }.count()
+        if (!modules.isEmpty()) {
+            modules.forEach(
+                Consumer { svnSubMod: SvnModule ->
+                    svnSubMod.subModules.addAll(
+                        listModulesSVN(svnInfo, repo, svnSubMod, level + 1, maxDepth)!!)
+                })
+        }
+        if (modulesFounds.stream().anyMatch { m: SvnModule? -> m is FakeModule }) {
+            return modulesFounds
+        }
+        log.debug("SVN modules found in {} : {}", module, modules)
+        return modules
+    }
+}

fa2dbb582986a99c2e25d5b53597b8bdc9e3b182
Mat V <mvincent.it@gmail.com>
2021-02-04 20:54:16 +0100

    🔧 Fix cleanup

--- src/main/kotlin/fr/yodamad/svn2git/init/CheckUp.kt
+++ src/main/kotlin/fr/yodamad/svn2git/init/CheckUp.kt
@@ -73,7 +73,7 @@ class CheckUp {
     private fun checkGitSvnClone(): Boolean {
         val result = execCommand("git --version")
         val regex = "git version 2\\.[2-3][0-9]\\.[0-9]".toRegex()
-        return if (regex.matches(result)) true
+        return if (regex.containsMatchIn(result)) true
         else {
             LOG.error(GIT_ERROR)
             false

27dc48cb5b44dbc5109ce63386842e6215037e68
yodamad <mvincent.it@gmail.com>
2023-05-16 10:40:44 +0200

    🧼 Remove authentication on navbar

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiLanguageService } from 'ng-jhipster';
-
 import { VERSION } from 'app/app.constants';
-import { JhiLanguageHelper, Principal, LoginModalService, LoginService } from 'app/core';
+import { JhiLanguageHelper } from 'app/core';
 import { ProfileService } from '../profiles/profile.service';
 
 @Component({
@@ -17,15 +15,11 @@ export class NavbarComponent implements OnInit {
     isNavbarCollapsed: boolean;
     languages: any[];
     swaggerEnabled: boolean;
-    modalRef: NgbModalRef;
     version: string;
 
     constructor(
-        private loginService: LoginService,
         private languageService: JhiLanguageService,
         private languageHelper: JhiLanguageHelper,
-        private principal: Principal,
-        private loginModalService: LoginModalService,
         private profileService: ProfileService,
         private router: Router
     ) {
@@ -53,25 +47,12 @@ export class NavbarComponent implements OnInit {
         this.isNavbarCollapsed = true;
     }
 
-    isAuthenticated() {
-        return this.principal.isAuthenticated();
-    }
-
-    login() {
-        this.modalRef = this.loginModalService.open();
-    }
-
     logout() {
         this.collapseNavbar();
-        this.loginService.logout();
         this.router.navigate(['']);
     }
 
     toggleNavbar() {
         this.isNavbarCollapsed = !this.isNavbarCollapsed;
-    }
-
-    getImageUrl() {
-        return this.isAuthenticated() ? this.principal.getImageUrl() : null;
     }
 }
